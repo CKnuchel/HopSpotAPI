7eede46f309effe46c16249824616db24dc22831
CKnuchel <christoph.knuchel@gmail.com>
2026-01-23 16:12:08 +0100

    ADDED: Implemented Create Function

--- internal/repository/user_repository.go
+++ internal/repository/user_repository.go
@@ -1,1 +1,26 @@
 package repository
+
+import (
+	"context"
+	"hopSpotAPI/internal/domain"
+
+	"gorm.io/gorm"
+)
+
+type userRepository struct {
+	db *gorm.DB
+}
+
+// NewUserRepository Constructor for UserRepository
+func NewUserRepository(db *gorm.DB) UserRepository {
+	return &userRepository{db: db}
+}
+
+func (r userRepository) Create(ctx context.Context, user *domain.User) error {
+	return r.db.WithContext(ctx).Create(user).Error
+}
+
+func (r userRepository) FindByID(ctx context.Context, id uint) (*domain.User, error) {
+	//TODO https://gorm.io/docs/query.html#Retrieving-objects-with-primary-key
+	panic("implement me")
+}
