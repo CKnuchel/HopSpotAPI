71862351d9e7f287130a780b9461badf0e5966a9
CKnuchel <christoph.knuchel@gmail.com>
2026-01-23 16:11:51 +0100

    ADDED: First Interface for User Repo (Create, FindByID) added

--- internal/repository/interfaces.go
+++ internal/repository/interfaces.go
@@ -1,1 +1,11 @@
 package repository
+
+import (
+	"context"
+	"hopSpotAPI/internal/domain"
+)
+
+type UserRepository interface {
+	Create(ctx context.Context, user *domain.User) error
+	FindByID(ctx context.Context, id uint) (*domain.User, error)
+}
