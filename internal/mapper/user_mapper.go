f3e5b008cc19c893b6479ef3f951147e9fe04e4e
CKnuchel <christoph.knuchel@gmail.com>
2026-01-24 18:23:23 +0100

    ADDED: Implemented User Mapper

--- internal/mapper/user_mapper.go
+++ internal/mapper/user_mapper.go
@@ -1,1 +1,35 @@
 package mapper
+
+import (
+	"hopSpotAPI/internal/domain"
+	"hopSpotAPI/internal/dto/requests"
+	"hopSpotAPI/internal/dto/responses"
+	"strconv"
+)
+
+func RegisterRequestToUser(req *requests.RegisterRequest) *domain.User {
+	return &domain.User{
+		Email:       req.Email,
+		DisplayName: req.DisplayName,
+		// Password is hashed and set in the service layer
+		// Role is set to default in the service layer
+	}
+}
+
+func UserToResponse(user *domain.User) responses.UserResponse {
+	return responses.UserResponse{
+		ID:          strconv.Itoa(int(user.ID)),
+		Email:       user.Email,
+		DisplayName: user.DisplayName,
+		Role:        user.Role,
+		CreatedAt:   user.CreatedAt,
+	}
+}
+
+func UsersToResponses(users []domain.User) []responses.UserResponse {
+	responsesList := make([]responses.UserResponse, len(users))
+	for i, user := range users {
+		responsesList[i] = UserToResponse(&user)
+	}
+	return responsesList
+}
