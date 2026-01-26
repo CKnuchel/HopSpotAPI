38ebdf37090a67d989500c5939920a14e23e6b49
Christoph Knuchel <119663666+CKnuchel@users.noreply.github.com>
2026-01-26 07:19:55 +0000

    ADDED: Added register, login and health Endpoint

--- internal/router/router.go
+++ internal/router/router.go
@@ -1,1 +1,37 @@
 package router
+
+import (
+	"hopSpotAPI/internal/handler"
+	"hopSpotAPI/internal/middleware"
+
+	"github.com/gin-gonic/gin"
+)
+
+func Setup(authHandler *handler.AuthHandler, authMiddleware *middleware.AuthMiddleware) *gin.Engine {
+	router := gin.Default()
+
+	v1 := router.Group("/api/v1")
+	{
+
+		// Public routes
+		auth := v1.Group("/auth")
+		{
+			auth.POST("/register", authHandler.Register)
+			auth.POST("/login", authHandler.Login)
+		}
+
+		// Protected routes
+		protected := v1.Group("/")
+		protected.Use(authMiddleware.Authenticate())
+		{
+			// Place for protected routes
+		}
+	}
+
+	// Default routes
+	router.GET("/health", func(c *gin.Context) {
+		c.JSON(200, gin.H{"status": "healthy"})
+	})
+
+	return router
+}
