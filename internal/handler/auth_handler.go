c44cd4d72879bcf469205e29601d9a38218dfa02
Christoph Knuchel <119663666+CKnuchel@users.noreply.github.com>
2026-01-26 07:06:26 +0000

    ADDED: Registration handler

--- internal/handler/auth_handler.go
+++ internal/handler/auth_handler.go
@@ -1,1 +1,34 @@
 package handler
+
+import (
+	"hopSpotAPI/internal/dto/requests"
+	"hopSpotAPI/internal/service"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type AuthHandler struct {
+	authService service.AuthService
+}
+
+func NewAuthHandler(authService service.AuthService) *AuthHandler {
+	return &AuthHandler{authService: authService}
+}
+
+// POST /api/v1/auth/register
+func (h *AuthHandler) Register(c *gin.Context) {
+	var req requests.RegisterRequest
+	if err := c.ShouldBindJSON(&req); err != nil {
+		c.JSON(400, gin.H{"error": "Invalid request"})
+		return
+	}
+
+	result, err := h.authService.Register(c.Request.Context(), &req)
+	if err != nil {
+		c.JSON(400, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusCreated, result)
+}
