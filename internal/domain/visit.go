33dc7a6f6ee58240623ff084c47986d42a964d98
Christoph Knuchel <119663666+CKnuchel@users.noreply.github.com>
2026-01-26 07:31:59 +0000

    FIXED: Fixed data type of VisitedAt to Postgres type

--- internal/domain/visit.go
+++ internal/domain/visit.go
@@ -10,6 +10,6 @@ type Visit struct {
 	*gorm.Model
 	BenchID   uint      `gorm:"index:idx_bench,priority:1" json:"benchId"`
 	UserID    uint      `gorm:"index:idx_user,priority:1" json:"userId"`
-	VisitedAt time.Time `gorm:"type:datetime;not null" json:"visitedAt"`
+	VisitedAt time.Time `gorm:"type:timestamptz;not null" json:"visitedAt"`
 	Comment   string    `gorm:"type:varchar(255);not null" json:"comment"`
 }
