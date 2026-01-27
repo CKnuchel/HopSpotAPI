61aaef9b8f335639e84afe0b2c1a94cd89b7913d
CKnuchel <christoph.knuchel@gmail.com>
2026-01-27 18:40:03 +0100

    ADDED: Implemented adminService struct and methods for user and invitation code management

--- internal/service/admin_service.go
+++ internal/service/admin_service.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"hopSpotAPI/internal/dto/requests"
 	"hopSpotAPI/internal/dto/responses"
+	"hopSpotAPI/internal/repository"
 )
 
 type AdminService interface {
@@ -15,4 +16,41 @@ type AdminService interface {
 	// Invitation Codes
 	ListInvitationCodes(ctx context.Context, req *requests.ListInvitationCodesRequest) (*responses.PaginatedInvitationCodesResponse, error)
 	CreateInvitationCode(ctx context.Context, req *requests.CreateInvitationCodeRequest, adminID uint) (*responses.InvitationCodeResponse, error)
+}
+
+type adminService struct {
+	userRepo           repository.UserRepository
+	invitationCodeRepo repository.InvitationRepository
+}
+
+func NewAdminService(userRepo repository.UserRepository, invitationCodeRepo repository.InvitationRepository) AdminService {
+	return &adminService{
+		userRepo:           userRepo,
+		invitationCodeRepo: invitationCodeRepo,
+	}
+}
+
+// ListUsers implements [AdminService].
+func (a *adminService) ListUsers(ctx context.Context, req *requests.ListUsersRequest) (*responses.PaginatedUsersResponse, error) {
+	panic("unimplemented")
+}
+
+// UpdateUser implements [AdminService].
+func (a *adminService) UpdateUser(ctx context.Context, id uint, req *requests.AdminUpdateUserRequest) (*responses.UserResponse, error) {
+	panic("unimplemented")
+}
+
+// DeleteUser implements [AdminService].
+func (a *adminService) DeleteUser(ctx context.Context, id uint, adminID uint) error {
+	panic("unimplemented")
+}
+
+// ListInvitationCodes implements [AdminService].
+func (a *adminService) ListInvitationCodes(ctx context.Context, req *requests.ListInvitationCodesRequest) (*responses.PaginatedInvitationCodesResponse, error) {
+	panic("unimplemented")
+}
+
+// CreateInvitationCode implements [AdminService].
+func (a *adminService) CreateInvitationCode(ctx context.Context, req *requests.CreateInvitationCodeRequest, adminID uint) (*responses.InvitationCodeResponse, error) {
+	panic("unimplemented")
 }
