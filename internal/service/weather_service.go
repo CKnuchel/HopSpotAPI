1e163c328dc3098e12dcf8ee91ba14b9d5ba5ed6
Christoph Knuchel <119663666+CKnuchel@users.noreply.github.com>
2026-01-28 12:52:11 +0000

    feat: Implement WeatherService interface and weatherService struct for current weather retrieval

--- internal/service/weather_service.go
+++ internal/service/weather_service.go
@@ -1,1 +1,25 @@
 package service
+
+import (
+	"context"
+	"hopSpotAPI/internal/dto/responses"
+	"hopSpotAPI/pkg/weather"
+)
+
+type WeatherService interface {
+	GetCurrentWeather(ctx context.Context, lat, lon float64) (*responses.WeatherResponse, error)
+}
+
+type weatherService struct {
+	weatherClient *weather.WeatherClient
+}
+
+func NewWeatherService(weatherClient *weather.WeatherClient) WeatherService {
+	return &weatherService{
+		weatherClient: weatherClient,
+	}
+}
+
+func (s *weatherService) GetCurrentWeather(ctx context.Context, lat float64, lon float64) (*responses.WeatherResponse, error) {
+	return s.weatherClient.GetCurrentWeather(ctx, lat, lon)
+}
