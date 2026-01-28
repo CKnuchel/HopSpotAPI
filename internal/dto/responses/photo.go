a51259e9fc072acf8d9bd2e79451e2f59924911c
Christoph Knuchel <119663666+CKnuchel@users.noreply.github.com>
2026-01-28 10:31:53 +0000

    feat: Add PhotoResponse struct for photo data representation

---
+++ internal/dto/responses/photo.go
@@ -0,0 +1,14 @@
+package responses
+
+import "time"
+
+type PhotoResponse struct {
+	ID           uint      `json:"id"`
+	BenchID      uint      `json:"bench_id"`
+	IsMain       bool      `json:"is_main"`
+	URLOriginal  string    `json:"url_original,omitempty"`
+	URLMedium    string    `json:"url_medium,omitempty"`
+	URLThumbnail string    `json:"url_thumbnail,omitempty"`
+	UploadedBy   uint      `json:"uploaded_by"`
+	CreatedAt    time.Time `json:"created_at"`
+}
